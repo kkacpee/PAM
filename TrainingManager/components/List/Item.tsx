590c380735ead5716d6bc39ecfd65beed0859ab0
kamfel <kamil.fel76@gmail.com>
2020-12-27 15:57:02 +0100

    Some formatting Item.tsx

--- TrainingManager/components/List/Item.tsx
+++ TrainingManager/components/List/Item.tsx
@@ -1,45 +1,38 @@
 import React, { useState } from "react";
-import Swipeout from 'react-native-swipeout';
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View
-} from "react-native";
+import Swipeout from "react-native-swipeout";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
 
+export default function Item({ title }: { title: string }) {
+  const [btns] = useState([
+    {
+      text: "Delete",
+      backgroundColor: "red",
+      underlayColor: "rgba(0, 0, 0, 1, 0.6)",
+      //onPress: () => { this.deleteNote(rowData) }
+    },
+  ]);
 
-
-function Item({ title}: { title: string }) {
-  const [btns, setButtons] = useState([{
-    text: 'Delete',
-    backgroundColor: 'red',
-    underlayColor: 'rgba(0, 0, 0, 1, 0.6)'
-    //onPress: () => { this.deleteNote(rowData) }
-  }]);
-
-  return  <Swipeout right={btns}
-    autoClose= {true}
-    backgroundColor= 'transparent'>
-    <TouchableHighlight
-      underlayColor='rgba(192,192,192,1,0.6)'
-      //onPress={this.viewNote.bind(this, rowData)} 
+  return (
+    <Swipeout right={btns} autoClose={true} backgroundColor="transparent">
+      <TouchableHighlight
+        underlayColor="rgba(192,192,192,1,0.6)"
+        //onPress={this.viewNote.bind(this, rowData)}
       >
         <View style={styles.item}>
-            <Text style={styles.title}>{title}</Text>
+          <Text style={styles.title}>{title}</Text>
         </View>
-    </TouchableHighlight>
-  </Swipeout>
+      </TouchableHighlight>
+    </Swipeout>
+  );
 }
 
-  const styles = StyleSheet.create({
-    title: {
-        fontSize: 24,
-        color: "#ffa500"
-      },
-      item: {
-        backgroundColor: "#000",
-        padding: 20
-      },
-    });
-
-export default Item;
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    color: "#ffa500",
+  },
+  item: {
+    backgroundColor: "#000",
+    padding: 20,
+  },
+});
